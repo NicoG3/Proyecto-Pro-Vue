111cc9a43b3386b012138ff15bc9ee5666c8937c
DIAL\nicot <nicotg3@gmail.com>
2024-12-12 18:13:19 -0500

    Se crea componentes lista de tareas, se borran componenetes autogenerados y se crean rutas.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-<Contador></Contador>
+<MainView></MainView>
+
 </template>
 
 <script setup>
-import Contador from './components/Conta.vue';
+import MainView from './views/MainView.vue';
+//import Contador from './components/ListaTareasContador.vue';
+//import ListaDeTareas from './components/ListaTareas.vue'
 </script>
 
 
---
+++ src/components/ListaTSidebar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="sidebar">
+  <h2>Barra lateral</h2>
+  <ul>
+    <li>
+      <router-link to="/contador">Contador</router-link>
+    </li>
+    <li>
+      <router-link to="/lista-de-tareas">Lista</router-link>
+    </li>
+  </ul>
+
+
+
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.sidebar {
+  width: 200px;
+  background-color: wheat;
+  padding: 20px;
+}
+</style>
